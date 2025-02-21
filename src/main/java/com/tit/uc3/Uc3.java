599e8951a4dbb0de820d3dcb938e471a56043da2
rajharsh0205 <rajharsh810201@gmail.com>
2025-02-21 22:51:16 +0530

    [Harsh] Add . Uc3 feature Added

---
+++ src/main/java/com/tit/uc3/Uc3.java
@@ -0,0 +1,12 @@
+package com.tit.uc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc3 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uc3.class, args);
+    }
+}
