031c39d8be4c75d8c7dc76cb681acffb9236e61f
rajharsh0205 <rajharsh810201@gmail.com>
2025-02-21 22:23:56 +0530

    [Harsh] Add . Uc1 feature Added

---
+++ src/main/java/com/tit/uc1/Uc1Application.java
@@ -0,0 +1,13 @@
+package com.tit.uc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc1Application.class, args);
+	}
+
+}
